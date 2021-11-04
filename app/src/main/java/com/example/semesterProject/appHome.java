ee539211808e46d6b6b27f048c2c9c690e96fdf3
sarahworthington <smworthingto@wisc.edu>
2021-11-03 20:49:25 -0500

    menu

--- app/src/main/java/com/example/semesterProject/appHome.java
+++ app/src/main/java/com/example/semesterProject/appHome.java
@@ -1,5 +1,6 @@
 package com.example.semesterProject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -12,11 +13,12 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
-
 import com.google.android.material.navigation.NavigationBarView;
 
+
 public class appHome extends AppCompatActivity {
-    
+
+    private NavigationBarView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +27,8 @@ public class appHome extends AppCompatActivity {
         String servicesKey = "services";
         setContentView(R.layout.activity_notes_app);
 
-
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnItemSelectedListener(bottomnavFunction);
 
         SharedPreferences sharedPreferences = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
 
@@ -38,33 +41,56 @@ public class appHome extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.logout:
-                Intent intent = new Intent(this, LoginPage.class);
-                SharedPreferences sharedPreferences = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
-                sharedPreferences.edit().remove(LoginPage.usernameKey).apply();
-                sharedPreferences.edit().remove(LoginPage.servicesKey).apply();
-                startActivity(intent);
-                return true;
-            case R.id.streaming:
-                Intent intentChangeStreaming = new Intent(this, CreateAccount.class);
-                SharedPreferences sharedPreferencesServices = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
-                sharedPreferencesServices.edit().remove(LoginPage.servicesKey).apply();
-                startActivity(intentChangeStreaming);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.profile:
+                    fragment = new ProfileFragment();
+                    break;
+                case R.id.group:
+                    fragment = new GroupFragment();
+                    break;
+                case R.id.watchlist:
+                    fragment = new WatchlistFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
         }
-    }
+    };
+
+    //@TODO
+    //add streaming and logout to profile page
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.logout:
+//                Intent intent = new Intent(this, LoginPage.class);
+//                SharedPreferences sharedPreferences = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
+//                sharedPreferences.edit().remove(LoginPage.usernameKey).apply();
+//                sharedPreferences.edit().remove(LoginPage.servicesKey).apply();
+//                startActivity(intent);
+//                return true;
+//            case R.id.streaming:
+//                Intent intentChangeStreaming = new Intent(this, CreateAccount.class);
+//                SharedPreferences sharedPreferencesServices = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
+//                sharedPreferencesServices.edit().remove(LoginPage.servicesKey).apply();
+//                startActivity(intentChangeStreaming);
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 
 
 }
