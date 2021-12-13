f700ce2f97c460c3e172c8d569874e6873caf735
idcampbell <47904246+idcampbell@users.noreply.github.com>
2021-12-12 23:57:18 -0600

    added new recommendation algo

--- app/src/main/python/recommend.py
+++ app/src/main/python/recommend.py
@@ -1,11 +1,21 @@
 def helloworld():
     return 'Hello World!'
 
-def get_top_movies():
+def get_top_movies(genres, movies):
     from os.path import dirname, join
     from pandas import read_csv
     filename = join(dirname(__file__), 'movie_info.csv')
     movie_info = read_csv(filename)
+    # Edit names to get rid of apostrophes
+    movie_info['Title'] = movie_info['Title'].str.replace(r"[\"\',]", '')
+    # Filter based on genres.
+    genres = genres.split('  ')
+    genres = [genre.strip() for genre in genres]
+    good_inds = movie_info['Genre'].str.contains('|'.join(genres), case=False) #contains('|'.join(genres), case=False)
+    movie_info = movie_info[good_inds.values]
+    if len(movies)>0:
+        movies = movies.split('~')
+        movie_info = movie_info[~movie_info.Title.isin(movies)]
     movie_info = movie_info.sort_values('imdbRating', ascending=False)
     movies = Movies(movie_info.head(10))
     return movies #[['Title', 'Runtime', 'Genre', 'Plot', 'Poster']].values
