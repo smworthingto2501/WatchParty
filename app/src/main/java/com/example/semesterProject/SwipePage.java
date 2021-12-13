d495788961a6fe4e8550bbe78957b5a660b5b241
idcampbell <47904246+idcampbell@users.noreply.github.com>
2021-12-12 23:56:17 -0600

    added filtering functionality

--- app/src/main/java/com/example/semesterProject/SwipePage.java
+++ app/src/main/java/com/example/semesterProject/SwipePage.java
@@ -1,10 +1,12 @@
 package com.example.semesterProject;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.chaquo.python.PyObject;
@@ -19,8 +21,6 @@ import com.daprlabs.cardstack.SwipeDeck;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-// README:
 // This is part of the build out for swiping. This class is a part of that process, and follows this tutorial.
 // https://www.geeksforgeeks.org/tinder-swipe-view-with-example-in-android/
 public class SwipePage extends AppCompatActivity {
@@ -34,13 +34,53 @@ public class SwipePage extends AppCompatActivity {
     public int flag = 0; //flag for set as favorite button being clicked
     public int watched = 0; //flag for if the user has watched or not, used to adjust recommendation
 
-
-    //private MovieInfo movieInfo;
-
     public SwipePage() {
         // Required empty public constructor
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private ArrayList<SwipePage> getRecommendations() {
+        // Grab the user swipe list and genre preferences.
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabaseSwipes = context.openOrCreateDatabase("swipedList", Context.MODE_PRIVATE, null);
+        swipedListHelper swipedList = new swipedListHelper(sqLiteDatabaseSwipes);
+        String username = sharedPreferences.getString("username", "");
+        ArrayList<String> swipedSuccess = swipedList.readWatchlist(username);
+        //sqLiteDatabaseSwipes.close();
+        String swipedMovies = String.join("~", swipedSuccess);
+        Log.i("SWIPES", swipedMovies);
+        String genres = sharedPreferences.getString("genres", "");
+        Log.i("GENRES", genres);
+
+        Python python = Python.getInstance();
+        PyObject recommend = python.getModule("recommend");
+        PyObject movies = recommend.callAttr("get_top_movies", genres, swipedMovies);
+        String[] movieTitleArray = movies.get("titles").toJava(String[].class);
+        String[] movieRuntimeArray = movies.get("runtimes").toJava(String[].class);
+        String[] movieGenreArray = movies.get("genres").toJava(String[].class);
+        String[] moviePlotArray = movies.get("plots").toJava(String[].class);
+        String[] moviePosterArray = movies.get("posters").toJava(String[].class);
+        //movies.close();
+        ArrayList<String> movieTitles = new ArrayList<String>(Arrays.asList(movieTitleArray));
+        ArrayList<String> movieRuntimes = new ArrayList<String>(Arrays.asList(movieRuntimeArray));
+        ArrayList<String> movieGenres = new ArrayList<String>(Arrays.asList(movieGenreArray));
+        ArrayList<String> moviePlots = new ArrayList<String>(Arrays.asList(moviePlotArray));
+        ArrayList<String> moviePosters = new ArrayList<String>(Arrays.asList(moviePosterArray));
+
+        for (int i=0; i<movieTitles.size(); i++) {
+            movieModalArrayList.add(new SwipePage(
+                    movieTitles.get(i),
+                    movieRuntimes.get(i),
+                    movieGenres.get(i),
+                    moviePlots.get(i),
+                    R.drawable.ic_baseline_back_hand_24));
+        }
+        return movieModalArrayList;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,34 +93,11 @@ public class SwipePage extends AppCompatActivity {
         if (!Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
-        //movieInfo = new MovieInfo();
-
 
         // on below line we are initializing our array list and swipe deck.
         movieModalArrayList = new ArrayList<>();
         cardStack = (SwipeDeck) findViewById(R.id.swipe_deck);
-        Python python = Python.getInstance();
-        PyObject recommend = python.getModule("recommend");
-        PyObject movies = recommend.callAttr("get_top_movies");
-        String[] movieTitleArray = movies.get("titles").toJava(String[].class);
-        ArrayList<String> movieTitles = new ArrayList<String>(Arrays.asList(movieTitleArray));
-        String[] movieRuntimeArray = movies.get("runtimes").toJava(String[].class);
-        ArrayList<String> movieRuntimes = new ArrayList<String>(Arrays.asList(movieRuntimeArray));
-        String[] movieGenreArray = movies.get("genres").toJava(String[].class);
-        ArrayList<String> movieGenres = new ArrayList<String>(Arrays.asList(movieGenreArray));
-        String[] moviePlotArray = movies.get("plots").toJava(String[].class);
-        ArrayList<String> moviePlots = new ArrayList<String>(Arrays.asList(moviePlotArray));
-        String[] moviePosterArray = movies.get("posters").toJava(String[].class);
-        ArrayList<String> moviePosters = new ArrayList<String>(Arrays.asList(moviePosterArray));
-
-        for (int i=0; i<movieTitles.size(); i++) {
-            movieModalArrayList.add(new SwipePage(
-                    movieTitles.get(i),
-                    movieRuntimes.get(i),
-                    movieGenres.get(i),
-                    moviePlots.get(i),
-                    R.drawable.ic_baseline_back_hand_24));
-        }
+        movieModalArrayList = getRecommendations();
 
         // creating a variable for our adapter class and passing array list to it.
         final SwipeAdapter adapter = new SwipeAdapter(movieModalArrayList, this);
@@ -112,6 +129,8 @@ public class SwipePage extends AppCompatActivity {
                 ArrayList<String> swipedSuccess = swipedList.readWatchlist(username);
 
                 Log.i("SWIPES", swipedSuccess.toString());
+                //sqLiteDatabaseSwipes.close();
+                //movieModalArrayList.remove(position);
             }
 
             @Override
@@ -124,6 +143,7 @@ public class SwipePage extends AppCompatActivity {
 
                 // on card swiped to right we are displaying a toast message.
                 Toast.makeText(SwipePage.this, "Card Swiped Right", Toast.LENGTH_SHORT).show();
+                //movieModalArrayList.add(movieModalArrayList.get(position));
 
                 //this is how we can get data on the movie that they swiped
                 String title = movieModalArrayList.get(position).getMovieName();
@@ -131,12 +151,14 @@ public class SwipePage extends AppCompatActivity {
 
                 SharedPreferences sharedPreferences = getSharedPreferences("com.example.semesterProject", Context.MODE_PRIVATE);
                 String username = sharedPreferences.getString("username", "");
+
                 //add movie to watchlist
                 Context context = getApplicationContext();
                 SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("watchlist", Context.MODE_PRIVATE, null);
                 watchlistHelper watchlistHelper = new watchlistHelper(sqLiteDatabase);
                 watchlistHelper.saveWatchList(username, title);
                 ArrayList<String> saveSuccess = watchlistHelper.readWatchlist(username);
+                sqLiteDatabase.close();
 
                 Log.i("MOVIES", saveSuccess.toString());
 
@@ -144,12 +166,18 @@ public class SwipePage extends AppCompatActivity {
                 swipedListHelper swipedList = new swipedListHelper(sqLiteDatabaseSwipes);
                 swipedList.saveWatchList(username, title);
                 ArrayList<String> swipedSuccess = swipedList.readWatchlist(username);
+                //sqLiteDatabaseSwipes.close();
 
                 Log.i("SWIPES", swipedSuccess.toString());
+
+                // Remove from the deck.
+                //movieModalArrayList.remove(position);
             }
 
             @Override
             public void cardsDepleted() {
+                //movieModalArrayList = new ArrayList<>();
+                movieModalArrayList = getRecommendations();
                 // this method is called when no card is present
                 Toast.makeText(SwipePage.this, "No more cards present", Toast.LENGTH_SHORT).show();
             }
@@ -157,7 +185,7 @@ public class SwipePage extends AppCompatActivity {
             @Override
             public void cardActionDown() {
                 // this method is called when card is swiped down.
-                // This doesnt accurately report when cards are swiped up --Sarah
+                // This doesn't accurately report when cards are swiped up --Sarah
                 Log.i("TAG", "CARDS MOVED DOWN");
             }
 
